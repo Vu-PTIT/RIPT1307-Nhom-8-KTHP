4fdad87f129f656d29b1aeb4074a8ed7f40b3298
Lê Tuấn Vũ <vult.ptit@gmail.com>
2026-05-13 21:21:48 +0700

    feat: implement AvatarDropdown component with user profile display and logout functionality

--- base-web-umi/src/components/RightContent/AvatarDropdown.tsx
+++ base-web-umi/src/components/RightContent/AvatarDropdown.tsx
@@ -1,10 +1,8 @@
-import { landingUrl } from '@/services/base/constant';
-import { FileWordOutlined, GlobalOutlined, LogoutOutlined, UserOutlined } from '@ant-design/icons';
+import { LogoutOutlined, UserOutlined } from '@ant-design/icons';
 import { Avatar, Menu, Spin } from 'antd';
 import { type ItemType } from 'antd/lib/menu/hooks/useItems';
 import React from 'react';
-import { useModel } from 'umi';
-import { OIDCBounder } from '../OIDCBounder';
+import { history, useModel } from 'umi';
 import HeaderDropdown from './HeaderDropdown';
 import styles from './index.less';
 
@@ -13,9 +11,13 @@ export type GlobalHeaderRightProps = {
 };
 
 const AvatarDropdown: React.FC<GlobalHeaderRightProps> = ({ menu }) => {
-	const { initialState } = useModel('@@initialState');
+	const { initialState, setInitialState } = useModel('@@initialState');
 
-	const loginOut = () => OIDCBounder?.getActions()?.dangXuat();
+	const loginOut = async () => {
+		localStorage.removeItem('token');
+		await setInitialState((s) => ({ ...s, currentUser: undefined }));
+		history.push('/user/login');
+	};
 
 	if (!initialState || !initialState.currentUser)
 		return (
@@ -31,33 +33,6 @@ const AvatarDropdown: React.FC<GlobalHeaderRightProps> = ({ menu }) => {
 
 	const items: ItemType[] = [
 		{
-			key: 'name',
-			icon: <UserOutlined />,
-			label: fullName,
-		},
-		// {
-		// 	key: 'password',
-		// 	icon: <SwapOutlined />,
-		// 	label: 'Đổi mật khẩu',
-		// 	onClick: () => {
-		// 		const redirect = window.location.href;
-		// 		window.location.href = `${keycloakAuthEndpoint}?client_id=${AppModules[currentRole].clientId}&redirect_uri=${redirect}&response_type=code&scope=openid&kc_action=UPDATE_PASSWORD`;
-		// 	},
-		// },
-		{
-			key: 'office',
-			icon: <FileWordOutlined />,
-			label: 'Office 365',
-			onClick: () => window.open('https://office.com/'),
-		},
-		{
-			key: 'portal',
-			icon: <GlobalOutlined />,
-			label: APP_CONFIG_TITLE_LANDING ?? 'Cổng thông tin',
-			onClick: () => window.open(landingUrl),
-		},
-		{ type: 'divider', key: 'divider' },
-		{
 			key: 'logout',
 			icon: <LogoutOutlined />,
 			label: 'Đăng xuất',
@@ -65,15 +40,6 @@ const AvatarDropdown: React.FC<GlobalHeaderRightProps> = ({ menu }) => {
 			danger: true,
 		},
 	];
-
-	if (menu && !initialState.currentUser.realm_access?.roles?.includes('QUAN_TRI_VIEN')) {
-		// items.splice(1, 0, {
-		//   key: 'center',
-		//   icon: <UserOutlined />,
-		//   label: 'Trang cá nhân',
-		//   onClick: () => history.push('/account/center'),
-		// });
-	}
 
 	return (
 		<>
